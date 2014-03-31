2be489b8af0a8a4157eb467b914736c265535bf0
Matthias De Geyter <matthias.degeyter@ibbt.be>
2014-03-31 18:39:30 +0200

    removed console.log

--- epileptic-frontend/js/main.js
+++ epileptic-frontend/js/main.js
@@ -5,22 +5,22 @@ var intervalId;
 function init() {
     output = $("#color");
     socket =  io.connect("http://"+window.location.hostname);
-    
+
     blink("#FF0000", 100, 600);
 
     socket.on('data', onDataEvent);
 }
 
 function onDataEvent(data) {
-    console.log(data);
+    // console.log(data);
     blink(data.color, data.duration, data.interval);
     //socket.emit('my other event', { my: 'data' });
 }
 
 function blink(color, duration, interval) {
     clearRequestInterval(intervalId);
-    intervalId = requestInterval(function() { 
-        flash(color, duration); 
+    intervalId = requestInterval(function() {
+        flash(color, duration);
     }, interval);
 }
 
